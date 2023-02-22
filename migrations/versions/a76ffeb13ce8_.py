bfbf354f727d0b2b8e621165fba8c0d44cf3b866
Richard Diaz <richard.sd0007@gmail.com>
2023-02-22 12:58:49 -0500

    fix migration for production

--- migrations/versions/a76ffeb13ce8_.py
+++ migrations/versions/a76ffeb13ce8_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: a76ffeb13ce8
-Revises: 
+Revises:
 Create Date: 2023-02-22 12:40:56.821688
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -50,6 +54,11 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE notebooks SET SCHEMA {SCHEMA}")
+        op.execute(f"ALTER TABLE notes SET SCHEMA {SCHEMA}")
     # ### end Alembic commands ###
 
 

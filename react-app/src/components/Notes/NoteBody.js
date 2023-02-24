69065426267579fe47e593ada7fcb60df5f00f56
Richard Diaz <richard.sd0007@gmail.com>
2023-02-23 19:32:56 -0500

    fix note showing up

--- react-app/src/components/Notes/NoteBody.js
+++ react-app/src/components/Notes/NoteBody.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSingleNote, editNote } from "../../store/notes";
 
 function NoteBody({ note }) {
+  const user = useSelector(state => state.session.user);
+  console.log("USER:", user);
   const dispatch = useDispatch();
   const params = useParams();
-  const [title, setTitle] = useState(note.title)
-  const [body, setBody] = useState(note.body)
+  const [title, setTitle] = useState(note?.title);
+  const [body, setBody] = useState(note?.body);
   console.log("NOTEID: ", params.noteId);
   console.log("SINGLE NOTE: ", note);
+  console.log("NOTE TITLE:", note.title);
+  console.log("TITLE:", title);
 
   const date = new Date();
   const dateStr = date.toISOString();
@@ -20,6 +24,11 @@ function NoteBody({ note }) {
     }
   }, [dispatch, params.noteId, note.id])
 
+  useEffect(() => {
+    setTitle(note.title);
+    setBody(note.body);
+  }, [note]);
+
   const updateNote = () => {
     console.log("RUNNING UPDATE");
     dispatch(editNote(note.id, {
@@ -29,18 +38,17 @@ function NoteBody({ note }) {
   }
 
   return (
-    <div className="notebody-wrapper">
+    <div className="notebody-wrapper"
+    onMouseLeave={updateNote}>
       <textarea
       className="notebody-title"
       value={title}
       onChange={e => setTitle(e.target.value)}
-      onMouseLeave={updateNote}
       />
       <textarea
       className="notebody-body"
       value={body}
       onChange={e => setBody(e.target.value)}
-      onMouseLeave={updateNote}
       />
     </div>
   )
